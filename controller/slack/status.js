15136e09b94e2ab591c2455ae776c2b287aa6f59
blheson <blessingworld007@gmail.com>
2021-08-13 18:36:19 +0100

    bug fix

--- controller/slack/status.js
+++ controller/slack/status.js
@@ -1,4 +1,5 @@
 const { sendMessage } = require('./send_message');
+const fetch = require('node-fetch');
 const endpoint = process.env.endpoint || 'https://immense-journey-49318.herokuapp.com/api/v1/health'
 const hourTimer = 60000 //per hour;
 module.exports = status = async (req, res, next) => {
@@ -8,20 +9,23 @@ module.exports = status = async (req, res, next) => {
     res.sendStatus(200);
 
     if (payload.event.type === "app_mention") {
-        if (payload.event.text.includes("start server check")) {
+        
+        let botIntervalTimer;
+        if (payload.event.text.includes("start")) {
             // Make call to chat.postMessage using bot's token
             botIntervalTimer = setInterval(async () => {
                 let response = await fetch(endpoint);
                 let result = await response.json();
                 let feedback = await sendMessage(result.message);
-                console.log(feedback, result.message);
+                console.log(feedback);
             }, hourTimer);
-        } else if (payload.event.text.includes("stop server check")) {
+        } else if (payload.event.text.includes("stop")) {
             clearInterval(botIntervalTimer);
             let feedback = await sendMessage("I have stopped checking the server's health");
-            console.log(feedback, result.message);
-        }else{
+            console.log(feedback);
+        } else {
             let feedback = await sendMessage(`I do not recognise "${payload.event.text}" command`);
+            console.log(feedback);
         }
     }
     // let botIntervalTimer;
